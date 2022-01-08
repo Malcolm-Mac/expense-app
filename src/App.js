1f645cdb0e0ca72ec5760ea0a44df70ffdd73d69
Malcolm-Mac <malcolmprojects12@gmail.com>
2022-01-08 15:04:41 +0200

    malcolm separated expense-items into expenses

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import ExpenseItems from './components/ExpenseItems';
+import Expenses from './components/Expenses';
 
-function App() {
-  const expenses = [
+function App(props) {
+   props = [
     { id: 'e1', title: 'Toilet Paper', amount: 94.12, date: new Date(2020, 7, 14) },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     { id: 'e3', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
@@ -11,10 +11,7 @@ function App() {
   return (
     <div className="App">
 
-      <ExpenseItems title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItems>
-      <ExpenseItems title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItems>
-      <ExpenseItems title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItems>
-      <ExpenseItems title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItems>
+      <Expenses items={props} />
 
     </div>
   );
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,21 +1,18 @@
 import './ExpenseItems.css';
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItems(props) {
-    const month = props.date.toLocaleString('en-US',{ month:'long' });
-    const day = props.date.toLocaleString('en-US',{ day:'2-digit' });
-    const year = props.date.getFullYear();
 
     return (
         <div className='expense-item'>
-            <div>{ month }</div>
-            <div>{ year }</div>
-            <div>{ day }</div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>R{props.amount}</div>
             </div>
         </div>
     );
+
 }
 
-export default ExpenseItems
+export default ExpenseItems;
